7c2a3dc8e05648efa4f40eb87bf6822b01355719
MARCO LOPEZ <mlopez@virtualsip.net>
2013-07-01 00:47:12 -0500

    datos del diario y codigo de google analytics

--- wg-footer.php
+++ wg-footer.php
@@ -2,7 +2,7 @@
 
     <div class="cnt-footer">
 
-    <p> © Empresa Editora Diario16 </p>
+    <p> © Diario16 </p>
   
     <div class="searcher box_busqueda">
         <form action="buscar.php" method="get" name="busqueda" id="busqueda">
@@ -28,21 +28,32 @@
 
     </div><!-- FIN SECCIONES ECO -->
     
-    <!--
+    
     <div class="mails">
         <ul>
-            <li>Director: <a href="director.html"></a></li>
-            <li>Archivo Periodístico: <a href="mailto:"></a></li>
-            <li>Publicidad Online: <a href="mailto:"></a></li>
-            <li>Redacción Online: <a href="mailto:"></a></li>
-            <li>Archivo Histórico: <a href="mailto:"></a></li>
-            <li>Webmaster: <a href="mailto:"></a></li>
-            <li>Redacción Ed. Impresa: <a href="mailto:"></a></li>
-            <li>Suscripciones: <a href="mailto:"></a></li>
+            <li>Jefe de Publicidad: <a href="mailto:foliva@diario16.com.pe">Franco Oliva</a></li>
+            <li>Telefonos: RPM# 433599 - Central 4213883 Anexo 116</li>
+            <li>Si tienes algo que denunciar escribe a <a href="mailto:denuncias@diario16.com.pe">denuncias@diario16.com.pe</a> / 421-3883</li>
+            <li>Central : 4401712 / 4212564 * <a href="mailto:prensa@diario16.com.pe">prensa@diario16.com.pe</a></li>
+            <li>Diario 16 es una publicación de SERVICIOS DE COMUNICACIONES ESPECIALIZADAS E INTEGRALES DEL PERU SAC</li>
+            <li><strong>© Copyright 2013</strong></li>
         </ul>
         <div class="clear"></div>
-    </div> FIN MAILS 
-    -->
-      
+    </div> <!-- FIN MAILS -->
+
   </div>
 </div><!-- FIN FOOTER -->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-20229980-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
