cf4212d2c85fb743eaad7bf1039d4971dbad2bb3
Marco Lopez <mlopez@virtualsip.net>
2013-07-12 14:39:44 -0500

    nueva funcion nombre de mes sin 0

--- panel@diario16/conexion/funciones.php
+++ panel@diario16/conexion/funciones.php
@@ -71,8 +71,7 @@ function codigoAleatorio($length=10,$uc=TRUE,$n=TRUE,$sc=FALSE)
 	return $rstr;
 }
 
-function nombreMes($numero_mes)
-{
+function nombreMes($numero_mes){
 	switch($numero_mes){
    		case 01: return "Enero"; break;
 		case 02: return "Febrero"; break;
@@ -83,6 +82,23 @@ function nombreMes($numero_mes)
 		case 07: return "Julio"; break;
 		case 08: return "Agosto"; break;
 		case 09: return "Septiembre"; break;
+		case 10: return "Octubre"; break;
+		case 11: return "Noviembre"; break;
+		case 12: return "Diciembre"; break;
+	}
+}
+
+function nombreMesSC($numero_mes){
+	switch($numero_mes){
+   		case 1: return "Enero"; break;
+		case 2: return "Febrero"; break;
+		case 3: return "Marzo"; break;
+		case 4: return "Abril"; break;
+		case 5: return "Mayo"; break;
+		case 6: return "Junio"; break;
+		case 7: return "Julio"; break;
+		case 8: return "Agosto"; break;
+		case 9: return "Septiembre"; break;
 		case 10: return "Octubre"; break;
 		case 11: return "Noviembre"; break;
 		case 12: return "Diciembre"; break;
