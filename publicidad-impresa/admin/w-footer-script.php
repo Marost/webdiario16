2d5916f98a075d64af538d36fb796be764a66cd5
Marco Lopez <mlopez@marostdevelopers.com>
2014-08-12 18:02:32 -0500

    simplificar codigo en FOOTER y HEADER

---
+++ publicidad-impresa/admin/w-footer-script.php
@@ -0,0 +1,11 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.isotope.js"></script>
+<script src="js/jquery.fitvids.js"></script>
+<script src="js/fancybox/source/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/jquery.formLabels1.0.js"></script>
+<script src="js/jquery.jplayer.js"></script>
+<script src="js/jquery.ias.min.js" type="text/javascript"></script>
+<!--[if (gte IE 6)&(lte IE 8)]><script type="text/javascript" src="js/selectivizr-min.js"></script><![endif]-->
+<script defer src="js/scripts.js"></script>
---
+++ publicidad-impresa/admin/w-header-script.php
@@ -0,0 +1,12 @@
+<title>Publicidad en edición impresa | Diario16</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic|Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/default.css">
+<link rel="stylesheet" href="js/fancybox/source/jquery.fancybox.css">
+<link rel="stylesheet" href="/publicidad-impresa/css/font-awesome.min.css"/>
+
+<script src="js/modernizr.js"></script>
